8f54bc4582fcf22c3c912e88fdc8b3581e9cc2e7
Chau Duong <chau.duong1192@gmail.com>
2023-11-25 17:47:47 +0700

    fix: 🐛 remove .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,2 @@
 export const EXPO_PUBLIC_API_URL = process.env.EXPO_PUBLIC_API_URL as string;
 export const EXPO_PUBLIC_API_API = process.env.EXPO_PUBLIC_API_API as string;
-export const EXPO_NO_METRO_LAZY = process.env.EXPO_NO_METRO_LAZY as string;
